765253adddcfd23a82303514b9febf0a091dad7d
VladyslavHimself <vladyslavhimself@gmail.com>
2022-02-15 20:55:26 -0500

    Register form ready to api plugin

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,6 +1,7 @@
 import { Button, Flex } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { InputForm } from '../src/components/InputForm/component';
 import { InputField } from '../src/Ui/InputField/component';
 
@@ -8,15 +9,35 @@ const Home: NextPage = () => {
 
   const router = useRouter();
 
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [repeatPass, setRepeatPass] = useState<string>('');
+
   const onRegisterHandler = () => {
     router.push('/register');
   }
 
   return (
     <InputForm title='Register'>
-        <InputField name={'E-mail'} />
-        <InputField name={'Password'} type={'password'}/>
-        <InputField name={'Repeat password'} type={'password'}/>
+        <InputField
+         name={'E-mail'}
+         inputValue={email}
+         setInputValue={setEmail} 
+        />
+        
+        <InputField
+         name={'Password'}
+         type={'password'}
+         inputValue={password}
+         setInputValue={setPassword} 
+        />
+
+        <InputField
+         name={'Repeat password'}
+         type={'password'}
+         inputValue={repeatPass}
+         setInputValue={setRepeatPass} 
+        />
         
         <Flex w={'100%'} justifyContent={'center'}>
           <Button mt={'5px'} onClick={onRegisterHandler}> Sign Up </Button>
