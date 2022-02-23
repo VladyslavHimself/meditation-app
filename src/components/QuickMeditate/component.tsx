be811fb3b5391c88c12681aeda5c994538661267
VladyslavHimself <vladyslavhimself@gmail.com>
2022-02-23 01:25:09 -0500

    refactored

--- src/components/QuickMeditate/component.tsx
+++ src/components/QuickMeditate/component.tsx
@@ -8,8 +8,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const QuickMeditate = (): JSX.Element => {
 
-  const [minutes, setMinutes] = useState<number>(0);
-  const [seconds, setSeconds] = useState<number>(5);
+  const [minutes, setMinutes] = useState<number>(5);
+  const [seconds, setSeconds] = useState<number>(0);
   const [isTimerStarted, setIsTimerStarted] = useState<boolean>(false)
 
   const timer = new Timer();
@@ -20,7 +20,6 @@ export const QuickMeditate = (): JSX.Element => {
     } else if (time >= 10) {
       return time;
     };
-
     return time;
   }
 
@@ -37,23 +36,20 @@ export const QuickMeditate = (): JSX.Element => {
     }
   };
 
-  const saveMeditationInDB = () => {
-
-    const connectToFirestore = async () => {
+  const connectToFirestoreAndSaveMeditation = () => {
+    const callback = async () => {
       await setDoc(doc(db, localStorage.getItem('email')!, 'Total_data', 'meditations', uuidv4()), {
         createdAt: new Date(),
         minutes: 5,
       });
-    } 
+    };
 
-    connectToFirestore();
-    
-
+    callback();
   } 
 
   useEffect(() => {
     if (isTimerStarted) {
-     minutes <= 0 && seconds <= 0 ? saveMeditationInDB()
+     minutes <= 0 && seconds <= 0 ? connectToFirestoreAndSaveMeditation()
      : timer.tick(seconds, minutes, setSeconds, setMinutes);
     }
   }, [minutes, seconds, isTimerStarted])
