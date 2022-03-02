5aea44864e822097d863c09b103c655d053d996d
VladyslavHimself <vladyslavhimself@gmail.com>
2022-03-01 23:17:32 -0500

    added timerSettings page

---
+++ pages/timerSettings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const TimerSettings: NextPage = () => {
+
+
+  return (
+    <h1>Next page</h1>
+  )
+}
+
+export default TimerSettings;
