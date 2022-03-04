43b1eaaec4d0b581a44ccb1baa7ef4b8f144d641
VladyslavHimself <vladyslavhimself@gmail.com>
2022-03-03 22:53:25 -0500

    add time converter in meditate page

--- pages/meditate.tsx
+++ pages/meditate.tsx
@@ -1,29 +1,24 @@
 import React, { useEffect, useState } from 'react';
-
 import { NextPage } from 'next';
 import Image from 'next/Image';
-
 import BackgroundLayout from '../src/Layouts/BackgroundLayout';
-
 import { Navbar } from '../src/components/Navbar/component';
-
-import {Flex, Text, Input, Box} from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import { GenButton } from '../src/Ui/GenButton/component';
-
 import classes from '../src/scss/meditate.module.scss';
-
 import pauseIcon from '../src/assets/pause.svg';
-import { useRouter } from 'next/router';
+import { QuickMeditate } from '../src/components/QuickMeditate/component';
 
 const Meditate: NextPage = () => {
 
-  const [timerValue, setTimerValue] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0)
 
   useEffect(() => {
     const currentUrl = window.location.search;
     const data: string = currentUrl && new URLSearchParams(currentUrl).get('time')!;
-    data && setTimerValue(+data);
-  }, [timerValue]);
+    data && setMinutes(+data);
+  }, []);
 
   return (
     <BackgroundLayout>
@@ -32,7 +27,11 @@ const Meditate: NextPage = () => {
         <Text fontSize={'5xl'} color={'white'}>Keep calm & focus in your breate...</Text>
         <div className={classes.timer}>
           <div className={classes['circular-progress']}>
-            <div className={classes['value-container']}>{ timerValue }</div>
+            <div className={classes['value-container']}>
+              { minutes.toString().length > 1 ? minutes : `0${minutes}` }
+               :
+               { seconds.toString().length > 1 ? seconds : `0${seconds}` }
+            </div>
           </div>
         </div>
 
