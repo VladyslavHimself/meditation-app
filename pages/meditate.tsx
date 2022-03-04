046d11f5d56366d8f928f0392b48bc9cdb0ce92e
VladyslavHimself <vladyslavhimself@gmail.com>
2022-03-03 22:31:34 -0500

    now meditate page can receive information from config

--- pages/meditate.tsx
+++ pages/meditate.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { NextPage } from 'next';
 import Image from 'next/Image';
@@ -13,8 +13,17 @@ import { GenButton } from '../src/Ui/GenButton/component';
 import classes from '../src/scss/meditate.module.scss';
 
 import pauseIcon from '../src/assets/pause.svg';
+import { useRouter } from 'next/router';
 
 const Meditate: NextPage = () => {
+
+  const [timerValue, setTimerValue] = useState<number>(0);
+
+  useEffect(() => {
+    const currentUrl = window.location.search;
+    const data: string = currentUrl && new URLSearchParams(currentUrl).get('time')!;
+    data && setTimerValue(+data);
+  }, [timerValue]);
 
   return (
     <BackgroundLayout>
@@ -23,7 +32,7 @@ const Meditate: NextPage = () => {
         <Text fontSize={'5xl'} color={'white'}>Keep calm & focus in your breate...</Text>
         <div className={classes.timer}>
           <div className={classes['circular-progress']}>
-            <div className={classes['value-container']}>5:00</div>
+            <div className={classes['value-container']}>{ timerValue }</div>
           </div>
         </div>
 
